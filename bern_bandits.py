862eb08130c601471c3893227c2412297c02f89e
YoungseogChung <youngsec@andrew.cmu.edu>
2019-04-10 21:58:13 -0700

    update

--- bern_bandits.py
+++ bern_bandits.py
@@ -19,12 +19,14 @@ def plot_regret(instant_regret, plot_type):
         plt.ylabel('Instant regret')
     elif plot_type == 'cumulative':
         cum_regret = np.cumsum(instant_regret)
-        plt.plot(timesteps, cum_regret, '-bo')
+        log_cum_regret = np.log(cum_regret)
+        plt.plot(timesteps, log_cum_regret, '-bo')
         plt.xlabel('Timesteps')
         plt.ylabel('Cumulative regret')
     elif plot_type == 'avg_cum':
         avg_cum_regret = np.cumsum(instant_regret)/np.array(timesteps)
-        plt.plot(timesteps, avg_cum_regret, '-bo')
+        log_avg_cum_regret = np.log(avg_cum_regret)
+        plt.plot(timesteps, log_avg_cum_regret, '-bo')
         plt.xlabel('Timesteps')
         plt.ylabel('Average Cumulative regret')
 
@@ -56,14 +58,14 @@ class DiscreteBandits(object):
 
 
 class BernoulliBandits(DiscreteBandits):
-    def __init__(self, num_arms, prior_distr_type, init_pulls=10):
+    def __init__(self, num_arms, prior_distr_type, init_pulls=1):
         super(BernoulliBandits, self).__init__(num_arms, prior_distr_type,
                                                init_pulls)
         self._setup_priors()
         self._setup_arms()
 
     def _setup_arms(self):
-        # np.random.seed(1234)
+        np.random.seed(1234)
         self.true_arm_means = np.random.rand(self.num_arms)
         self.best_arm = np.argmax(self.true_arm_means)
         self.best_arm_mean = np.max(self.true_arm_means)
@@ -144,21 +146,42 @@ class BernoulliBandits(DiscreteBandits):
         self._post_arm_pull(arm_idx, current_reward)
         self._update_distr(arm_idx,current_reward)
 
-def main():
-    test_bern_bandit = BernoulliBandits(1000,'beta')
-    for i in range(1000):
-        # test_bern_bandit.pull_arm_freq_greedy()
-        # test_bern_bandit.pull_arm_bayesian_greedy()
-        # test_bern_bandit.pull_arm_TS()
-    print(test_bern_bandit.true_arm_means)
-    print(np.array(test_bern_bandit.arm_pulls))
-    plot_regret(test_bern_bandit.instant_regret,'avg_cum')
+    def pull_arm_UCB1(self):
+        if self.arm_pulls[0] < self.init_pulls:
+            self._init_pulls()
 
-    # test_bern_bandit = BernoulliBandits(4,'beta')
-    # for i in range(1000):
-    #     test_bern_bandit.pull_arm_greedy()
-    # print(test_bern_bandit.true_arm_means)
-    # print(np.array(test_bern_bandit.arm_pulls))
+        # import pdb; pdb.set_trace()
+
+        current_timestep = np.sum(self.arm_pulls) + 1
+        current_UCB = np.array(self.arm_rewards)/np.array(self.arm_pulls) + \
+                      np.sqrt((2*np.log(current_timestep))/self.arm_pulls)
+        arm_idx = np.argmax(current_UCB)
+        current_reward = np.random.binomial(1,self.true_arm_means[arm_idx],1)[0]
+
+        self._post_arm_pull(arm_idx, current_reward)
+        self._update_distr(arm_idx,current_reward)
+
+def main():
+    ucb_bandit = BernoulliBandits(10,'beta',1)
+    ts_bandit = BernoulliBandits(10,'beta',1)
+    freq_greedy_bandit = BernoulliBandits(10,'beta',1)
+    bayes_greedy_bandit = BernoulliBandits(10,'beta',1)
+    for i in range(100):
+        # test_bern_bandit.pull_arm_freq_greedy()
+        # bayes_greedy_bandit.pull_arm_bayesian_greedy()
+        # test_bern_bandit.pull_arm_TS()
+        ucb_bandit.pull_arm_UCB1()
+        ts_bandit.pull_arm_TS()
+        freq_greedy_bandit.pull_arm_freq_greedy()
+        bayes_greedy_bandit.pull_arm_bayesian_greedy()
+
+    print(ucb_bandit.true_arm_means)
+    # print(np.array(ucb_bandit.arm_pulls))
+    plot_regret(ucb_bandit.instant_regret,'cumulative')
+    plot_regret(ts_bandit.instant_regret,'cumulative')
+    plot_regret(freq_greedy_bandit.instant_regret,'cumulative')
+    plot_regret(bayes_greedy_bandit.instant_regret,'cumulative')
+
 
 if __name__ == '__main__':
     main()
